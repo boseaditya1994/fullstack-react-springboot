7b6528b613233bdf71b206cd38b5f26b5713a898
boseaditya1994 <boseaditya1994@gmail.com>
2026-02-14 17:34:36 +0530

    H2 Database setup

--- eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -2,12 +2,11 @@ package com.eazybytes.eazystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class EazystoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EazystoreApplication.class, args);
 	}
-
 }
